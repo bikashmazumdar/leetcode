63a5877430f1c80c67bf4e44ac6e3aa96662a97a
bikashmazumdar <bikashmazumdar2000@gmail.com>
2024-02-27 05:19:28 +0530

    day2#Merge Linked List Cycle

---
+++ JavaCode/src/main/java/leet/code/top150/easy/LinkedListCycle.java
@@ -0,0 +1,46 @@
+package leet.code.top150.easy;
+
+/*
+    Ref - https://leetcode.com/problems/linked-list-cycle/
+
+ */
+
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+public class LinkedListCycle {
+
+     /*
+        slow and fast pointer approach
+        true - if they meet, false - otherwise
+      */
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode fast = head, slow = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(10);
+        head.next = new ListNode(20);
+        head.next.next = head;
+
+        LinkedListCycle linkedListCycle = new LinkedListCycle();
+        System.out.println(linkedListCycle.hasCycle(head));
+    }
+}
