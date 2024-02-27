a3e220da87bdd7f550b86ce9bfa2229bcf67e09a
bikashmazumdar <bikashmazumdar2000@gmail.com>
2024-02-27 05:30:45 +0530

    day2#Is same tree

---
+++ JavaCode/src/main/java/leet/code/top150/easy/SameTree.java
@@ -0,0 +1,38 @@
+package leet.code.top150.easy;
+
+public class SameTree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        return p.val == q.val &&
+                isSameTree(p.left, q.left)  &&
+                isSameTree(p.right, q.right);
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        SameTree sameTree = new SameTree();
+        System.out.println(sameTree.isSameTree(p, q));
+    }
+}
